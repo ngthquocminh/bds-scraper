a79f5aa25259b3bdd08855040f9819039a6bb206
Nguyen Thanh Quoc Minh <35948603+ngthquocminh@users.noreply.github.com>
2021-03-29 08:33:56 +0700

    updaye rabbit

--- reciever.py
+++ reciever.py
@@ -5,12 +5,12 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='worker1')
+    channel.queue_declare(queue='hello1')
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(queue='worker1', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='hello1', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
