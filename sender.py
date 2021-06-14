fdd6e2ce79a6c1439e00f1f667d277181caca868
Nguyen Thanh Quoc Minh <35948603+ngthquocminh@users.noreply.github.com>
2021-06-14 16:37:21 +0700

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 import pika
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+# parameters = pika.ConnectionParameters(host='localhost')
+credentials = pika.PlainCredentials('worker01', 'worker01--')
+parameters = pika.ConnectionParameters('18.217.53.191',
+                                       5672,
+                                       '/',
+                                       credentials)
+try:
 
-channel.queue_declare(queue='hello1')
+    connection = pika.BlockingConnection(parameters)
 
-channel.basic_publish(exchange='', routing_key='hello1', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
-connection.close()
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello1')
+
+    channel.basic_publish(exchange='', routing_key='hello1', body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
+    connection.close()
+
+except pika.exceptions.ProbableAuthenticationError:
+    print("Login failed")
+except pika.exceptions.AMQPConnectionError:
+    print("Can not reach the client")
+except:
+    print("Unknow error")
