b2ea00faaa829f40064eae93dc4ce69f25c70767
Nguyen Thanh Quoc Minh <35948603+ngthquocminh@users.noreply.github.com>
2021-06-26 20:41:37 +0700

    Update test_parser.py

--- WebApp/Controller/back-end/Workers/utility/test_parser.py
+++ WebApp/Controller/back-end/Workers/utility/test_parser.py
@@ -1,19 +1,16 @@
 from Workers.utility.Database import DBObject
-from Workers.utility.ParserEngine import parse
+from Workers.utility.ParserEngines import parse
 from Workers.models import Parser
 from Workers.serializers import ParserSerializer
 
 def doTestOnParser(dict_request:dict): 
-    try:
-
-        parser_set = dict_request["parser_set"]
-        list_post_url = dict_request["list_post_url"]
-
-        parser_set  = Parser.objects.raw("""SELECT * FROM Workers_parser WHERE site="{_parser_set}" """.format(_parser_set=parser_set))
-        serializers = ParserSerializer(parser_set, many=True)
-        database = DBObject()
-        list_html = database.query_html_db({"$or":[{"url_hash":url_hash} for url_hash in list_post_url]})
-        content = [parse(html) for html in list_html]
-        return {"code":404,"message":"successfull","content":content}
-    except Exception as e:
-        return {"code":200,"message":repr(e)}
+    # try:
+    list_post_url = dict_request["list_url_hash"]
+    model_name =  dict_request["model_name_for_all"] if "model_name_for_all" in dict_request else None
+    database = DBObject()
+    list_post_html = database.query_html_db({"$or":[{"url_hash":url_hash} for url_hash in list_post_url]})
+    content = parse(list_post_html,many=True, model_name=model_name)
+    
+    return {"code":404,"message":"successfull","content":content}
+    # except Exception as e:
+    #     return {"code":200,"message":repr(e)}
