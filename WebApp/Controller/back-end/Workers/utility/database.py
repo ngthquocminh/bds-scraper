f17904535ceb1b1b06fde7c5714144efa5b4fe3f
Nguyen Thanh Quoc Minh <35948603+ngthquocminh@users.noreply.github.com>
2021-06-26 20:35:16 +0700

    Update database.py

--- WebApp/Controller/back-end/Workers/utility/database.py
+++ WebApp/Controller/back-end/Workers/utility/database.py
@@ -1,43 +1,75 @@
+import re
 from pymongo import MongoClient
 from pprint import pprint
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
 import pymongo
 
-
 class MongoDB:
-
-
+    ASC = 1
+    DES = -1
     def __init__(self):
 
         self.client = MongoClient(
             "mongodb+srv://lvtn:minh1709@cluster0.978ef.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
         )
 
-        self.lvtn_database   = "lvtn_database"
+        self.main_database   = "lvtn_database"
         self.parsed_db_name  = "parsed_db"
         self.html_db_name    = "html_db"
 
-        self.db_parsed = self.client[self.lvtn_database][self.parsed_db_name]
-        self.db_html  =  self.client[self.lvtn_database][self.html_db_name]
+        self.db_parsed = self.client[self.main_database][self.parsed_db_name]
+        self.db_html  =  self.client[self.main_database][self.html_db_name]
 
-    def insert_parsed_data(self, json_row=None):
+    def insert_parsed_data(self, json_row=None, many=False):
         if json_row is None:
             return 
         result = self.db_parsed.insert_one(json_row)
         return result
 
+    def get_collection(self,collection_name:str):
+        return self.client[collection_name]
     
-    def insert_html_data(self, json_row=None):
+    def insert_html_data(self, json_row=None, many=False):
         if json_row is None:
             return 
         result = self.db_html.insert_one(json_row)
         return result
 
-    def query_html_db(self, query_dict: dict):        
-        return self.db_html.find(query_dict)
+    def query_html_db(self, query_dict: dict, limit=1,sort=None):    
+        res = self.db_html.find(query_dict,limit=limit)
+        if sort:
+            res = res.sort(sort)
+        return [i for i in res]
 
-    def query_parsed_db(self, query_dict: dict):        
-        return self.db_parsed.find(query_dict)
+    def query_parsed_db(self, query_dict: dict, limit=1, sort=None):   
+        res = self.db_parsed.find(query_dict,limit=limit)
+        if sort:
+            res = res.sort(sort)     
+        return [i for i in res]
+
+    def pprint(self, result: pymongo.results.InsertOneResult):
+        pprint(result)
+
+
+class DBObject: # Interface
+
+    def __init__(self):
+        self.db_object = MongoDB()
+
+    def get_collection(self,collection_name:str):
+        return self.db_object.get_collection(collection_name)  
+
+    def insert_parsed_data(self, json_row=None, many=False):
+        return self.db_object.insert_parsed_data(json_row, many)
+    
+    def insert_html_data(self, json_row=None, many=False):
+        return self.db_object.insert_html_data(json_row, many)
+
+    def query_html_db(self, query_dict: dict, limit=0, sort=None):        
+        return self.db_object.query_html_db(query_dict, limit=limit, sort=sort)
+
+    def query_parsed_db(self, query_dict: dict, limit=0, sort=None):        
+        return self.db_object.query_parsed_db(query_dict, limit=limit, sort=sort)
 
     def pprint(self, result: pymongo.results.InsertOneResult):
         pprint(result)
@@ -72,7 +104,7 @@ def save_to(data_row=None):
 def save_to_mongodb(data_row):
     if data_row is None:
         return
-    database = MongoDB()
+    database = DBObject()
     database.insert_to(data_row)
 
 def save_to_azure_cosmos(data_row):
@@ -98,8 +130,8 @@ class DBTarget():
         if data_row is None:
             return
         
-        if not isinstance(self.database, MongoDB):
-            self.database = MongoDB()
+        if not isinstance(self.database, DBObject):
+            self.database = DBObject()
 
         self.database.insert_to(data_row)
 
