cec7a9eb7cbbbd0946c47e73ad6fbb17b1562163
Nguyen Thanh Quoc Minh <35948603+ngthquocminh@users.noreply.github.com>
2021-07-30 21:03:21 +0700

    Update ParserEngines.py

--- ParserEngines.py
+++ ParserEngines.py
@@ -9,7 +9,48 @@ import hashlib
 from ParserObject import ParserObject
 from ParserModelSelector import ParserModelSelector
 from LibFunc import clean_trash
+from database import DBObject
 
+#=============================================================================================
+#=============================================================================================
+
+
+def parse(data, many:bool=False, model_name=None):
+    parser_model = None
+    if model_name:
+        parser_model=ParserModelSelector(model_key=model_name)
+        if not isinstance(parser_model.get_model(), pd.DataFrame):
+            parser_model = None
+
+    if isinstance(data,list) and many:
+        result = []
+        for post in data:
+            
+            engine = ParserEngines(post=post,parser_model=parser_model,test_mode=True)
+            _r = engine.process_post()
+            result.append(_r["doc"] if _r["code"] == "OK" else {})
+        return result
+    elif isinstance(data,dict):
+        
+        engine = ParserEngines(post=date, parser_model=parser_model,test_mode=True)
+        _r = engine.process_post()
+        return _r["doc"] if _r["code"] == "OK" else {}
+
+    return {}
+
+def doParse(dict_request:dict): 
+    print(dict_request)
+    try:
+        list_post_url = dict_request["list_url_hash"]
+        model_name =  dict_request["model_name_for_all"] if "model_name_for_all" in dict_request else None
+        database = DBObject()
+        list_post_html = database.query_html_db({"$or":[{"url_hash":url_hash} for url_hash in list_post_url]})
+        content = parse(list_post_html,many=True, model_name=model_name)
+        
+        return {"code":200,"message":"successfull","content":content}
+    except Exception as e:
+        traceback.print_exc()
+        return {"code":404,"message":"failed"}
 class ParserEngines(object):
     def __init__(self, post:dict, parser_model:ParserModelSelector, test_mode:bool=True):
         self.__post = post
