30cabde801f4c0a0da0580adafa8e00d7737f568
Nguyen Thanh Quoc Minh <35948603+ngthquocminh@users.noreply.github.com>
2021-07-31 13:47:59 +0700

    Update worker.py

--- worker.py
+++ worker.py
@@ -8,28 +8,46 @@ from reciever import message_loads
 # from batdongsan import BatDongSanCrawler
 
 
-def start_crawling(date_from=None, date_to=None, post_type=None, all_date:bool = False):
+def start_crawling(date_from=None, date_to=None, post_type=None, resume=False, all_date:bool = False):
     ""
-    crawler = BatDongSanCrawler(post_type=post_type)
+    crawler = BatDongSanCrawler(post_type=post_type, date_from=date_from, date_to=date_to, resume=resume)
     crawler.obtain_data()
 
-
-def start_parsing():
+def start_parsing(list_post, model_name, site, type):
     ""
-    doParse()
+    print("Go to start_parsing")
+
+    doParse(list_post, model_name, site=site, type=type, num=len(list_post))
 
 def main(params):
     try:
         if params["command"] == "crawl":
-            date_from = params["post-date"].split("_")[0]
-            date_to   = params["post-date"].split("_")[1]
-            post_type = params["type"]
-            start_crawling(date_from=date_from, date_to=date_to, post_type=post_type)
+            if "resume" in params and int(params["resume"]) == 1:
+                start_crawling(resume=True)
+            else:
+                date_from = params["post-date"].split("_")[0].replace("-","/")
+                date_to   = params["post-date"].split("_")[1].replace("-","/")
+                post_type = params["type"]
+                start_crawling(date_from=date_from, date_to=date_to, post_type=post_type)
+
         elif params["command"] == "parse":
-            list_post = params["post"].split("_")
-            start_parsing(list_post)
-        elif params["command"] == "resume":
-            ""
+            print("Go to parse")
+            file = open("parse_posts.data","r")
+            posts = file.read()
+            file.close()
+            list_post = posts.split("_")
+            if "resume" in params and int(params["resume"]) == 1:
+
+                start_parsing(list_post=list_post, resume=True)
+            else:
+                model     = params["model"]
+                site      = params["site"]
+                post_type = params["type"]
+                
+                print("NEW parse")
+            
+                start_parsing(list_post=list_post, model_name = model, site=site, type=post_type)
+
         return True
     except:
         traceback.print_exc()
