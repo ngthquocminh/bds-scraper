d1172f177d288ac034175e150937a89a857dc4a9
Nguyen Thanh Quoc Minh <35948603+ngthquocminh@users.noreply.github.com>
2021-08-08 16:43:43 +0700

    Create CrawlerObject.py

---
+++ Workers/CrawlerObject.py
@@ -0,0 +1,11 @@
+class CrawlerObject(object): 
+    def visit(self, current_url) -> tuple:       
+        pass
+    def obtain_data(self):
+        pass
+    def get_html_and_soup_from_url(self, url):
+        pass
+    def save_data(self):
+        pass
+    def rotate_ip(self):
+        pass
